2a6be3adee394eae5b3ee5bbded40778acac4c5b
xeniaweber <ksenia94weber@gmail.com>
2021-03-29 02:06:06 +0300

    Auto add runner via ssh pseudo terminal

--- gitlab-ci/autoaddrunner.sh
+++ gitlab-ci/autoaddrunner.sh
@@ -9,18 +9,10 @@ read vm_name
 
 EXT_IP=$(yc compute instance get --name $vm_name | sed -n '24p' | awk '{print $2}')
 
-echo "Add Runner"
-docker run -d --name gitlab-runner \
-    --restart always -v /srv/gitlab-runner/config:/etc/gitlab-runner -v /var/run/docker.sock:/var/run/docker.sockgitlab/gitlab-runner:latest
+echo "Send script to $vm_name"
+chmod +x addreg.sh
+scp addreg.sh yc-user@$EXT_IP
 
-echo "Register runner"
-docker exec -it gitlab-runner gitlab-runner register \
-    --url http://$EXT_IP/ \
-    --non-interactive \
-    --locked=false \
-    --name DockerRunner_$timestamp \
-    --executor docker \
-    --docker-image alpine:latest \
-    --registration-token $run_token \
-    --tag-list "linux,xenial,ubuntu,docker" \
-    --run-untagged
+echo "Force pseudo terminal allocation"
+ssh -t yc-user@$EXT_IP \
+./addreg.sh $EXT_IP $timestamp $run_token
